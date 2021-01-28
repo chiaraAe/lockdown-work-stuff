f75a028792af39f0d70be5f89ae68e05eab1b927
Chiara Aengevelt <chiara@aengevelt.com>
2021-01-28 16:32:27 +0100

    Added Seeding with 4 Users and 10 Items, Added Faker Gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Item.destroy_all
+User.destroy_all
+puts "Database is clean"
+
+user_1 = User.create!(
+    email: 'Chiara@chiara.com',
+    password: '123456'
+)
+puts "User #{user_1.email} created"
+
+user_2 = User.create!(
+    email: 'megan@megan.com',
+    password: '123456'
+)
+puts "User #{user_2.email} created"
+
+user_3 = User.create!(
+    email: 'huy@huy.com',
+    password: '123456'
+)
+puts "User #{user_3.email} created"
+
+user_4 = User.create!(
+    email: 'tobi@tobi.com',
+    password: '123456'
+)
+puts "User #{user_4.email} created"
+
+
+users = [user_1, user_2, user_3, user_4]
+
+puts "Creating items..."
+
+10.times do 
+    item = Item.create!(
+      category: ['Lamps','Office Tables', 'Office Chairs','Bookshelfs', 'Pets', 'Phone Booth'].sample,
+      title: Faker::Commerce.product_name,
+      description: Faker::ChuckNorris.fact,
+      price: Faker::Commerce.price,
+      user_id: users.sample.id,
+    )
+    puts "item #{item.id} was created in #{item.category}"
+end
+puts "Finished!"
