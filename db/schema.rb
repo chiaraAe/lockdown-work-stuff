b4520f22df187b94d5909453e91b08f2d10633b5
Tobias Mentz <tobiasmentz@yahoo.com>
2021-01-30 15:46:10 +0100

    added image_url:string to items model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_190030) do
+ActiveRecord::Schema.define(version: 2021_01_30_144417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_190030) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
