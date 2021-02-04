67130cbdfb5d3cce2e0a4ed2b9201d1964c525a0
Chiara Aengevelt <chiara@aengevelt.com>
2021-02-04 17:32:09 +0100

    Added Flatpickr with disabled dates to editview

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,21 +2,21 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 
-window.addEventListener("turbolinks:load", function() {
-  flatpickr(".datepicker", {
-    dateFormat: "Y-m-d",
-    // disable: [
-    //     {
-    //         from: "2021-01-01",
-    //         to: "2021-02-13"
-    //     },
-    //     {
-    //         from: "2025-09-01",
-    //         to: "2025-12-01"
-    //     }
-    // ]
-  });
-});
+// window.addEventListener("turbolinks:load", function() {
+//   flatpickr(".datepicker", {
+//     dateFormat: "Y-m-d",
+//     // disable: [
+//     //     {
+//     //         from: "2021-01-01",
+//     //         to: "2021-02-13"
+//     //     },
+//     //     {
+//     //         from: "2025-09-01",
+//     //         to: "2025-12-01"
+//     //     }
+//     // ]
+//   });
+// });
 
 
 window.addEventListener("turbolinks:load", function() {
@@ -25,7 +25,7 @@ window.addEventListener("turbolinks:load", function() {
 
   // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
-  const unavailableDates = JSON.parse(document.querySelector('#flat-booking-dates').dataset.unavailable)
+  const unavailableDates = JSON.parse(document.querySelector('#item-booking-dates').dataset.unavailable)
   endDateInput.disabled = true
 
 
