7f257438f302345d3ad3582388939e80eea8879a
Tobias Mentz <tobiasmentz@yahoo.com>
2021-02-06 10:43:34 +0100

    implemented search form with hidden section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,25 @@
 class PagesController < ApplicationController
   def home
     @items = Item.all
+    if params[:query].present?
+      sql_query = " \
+        title ILIKE :query \
+        OR category ILIKE :query \
+        OR description ILIKE :query \
+      "
+      @search_result = Item.where(sql_query, query: "%#{params[:query]}%")
+      if @search_result.length >= 1
+        @show_result_div = 'true'
+        @text = "You want #{params[:query]}? We got #{params[:query]}!"
+      else
+        @show_result_div = 'none'
+        @text = "Sorry, we don't offer #{params[:query]} yet."
+      end
+
+    else
+      @search_result = {}
+      @text = nil
+      @show_result_div = 'none'
+    end
   end
 end
